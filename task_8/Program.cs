ded884170b79c2cf0f58bfd50872eb3053051fa6
Bazhaeva Madina <bazhaevaweb@gmail.com>
2023-03-15 13:26:16 +0300

    Задача 8 - решение

--- task_8/Program.cs
+++ task_8/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите целое число и нажмите enter: ");
+string inputN = Console.ReadLine() ?? "";
+double numberN = Convert.ToDouble(inputN);
+int interim = 1;
+
+Console.Write($" {numberN} -> ");
+while (interim <= numberN)
+{
+    if (interim%2==0)
+    {
+        Console.Write($" {interim}, ");
+    }
+    interim=interim+1;
+}
