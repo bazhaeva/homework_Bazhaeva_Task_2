3a2a344d0d1be589cb78705c3f7ae22b0af6e50a
Bazhaeva Madina <bazhaevaweb@gmail.com>
2023-03-15 13:23:22 +0300

    Задача 6 - решение

--- task_6/Program.cs
+++ task_6/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите целое число и нажмите enter: ");
+string inputA = Console.ReadLine() ?? "";
+double a = Convert.ToDouble(inputA);
+//double res = a%2;
+if (a%2==0)
+{
+    Console.WriteLine($"Число {a} четное");
+}
+else
+{
+    Console.WriteLine($"Число {a} нечетное");
+}
