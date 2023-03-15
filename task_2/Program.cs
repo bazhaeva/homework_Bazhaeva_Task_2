e4ea3d160690a6e5ad55334414499fa3a414b819
Bazhaeva Madina <bazhaevaweb@gmail.com>
2023-03-15 13:09:27 +0300

    Задача 2 решение

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите целое число 1 и нажмите enter: ");
+string userInput1 = Console.ReadLine() ?? "";
+
+Console.Write("Введите целое число 2 и нажмите enter: ");
+string userInput2 = Console.ReadLine() ?? "";
+
+int number1 = int.Parse(userInput1);
+int number2 = int.Parse(userInput2);
+
+if (number1>number2)
+{
+    Console.WriteLine($"{number1}, {number2} -> max = {number1} , min = {number2}");
+}
+else
+{
+    Console.WriteLine($"{number1}, {number2} -> max = {number2}, min = {number1}");
+}
