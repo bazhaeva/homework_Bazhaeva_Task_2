70d71085c2453674cbb8b0eba0944074844ff069
Bazhaeva Madina <bazhaevaweb@gmail.com>
2023-03-15 13:12:55 +0300

    Задача 4 решение

--- task_4/Program.cs
+++ task_4/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и 
+// выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите целое число №1 и нажмите enter: ");
+string InputA = Console.ReadLine() ?? "";
+Console.WriteLine("Введите целое число №2 и нажмите enter: ");
+string InputB = Console.ReadLine() ?? "";
+Console.WriteLine("Введите целое число №3 и нажмите enter: ");
+string InputC = Console.ReadLine() ?? "";
+
+int a = Convert.ToInt32(InputA);
+int b = Convert.ToInt32(InputB);
+int c = Convert.ToInt32(InputC);
+
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write($" {a}, {b}, {c} -> max = {max}");
